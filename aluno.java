5b1f03d748711ad53332e8522254affce1008fa7
Dhominiif <88465403+Dhominiif@users.noreply.github.com>
2021-11-27 19:59:56 -0300

    Add files via upload

--- aluno.java
+++ aluno.java
@@ -1,13 +1,15 @@
 public class aluno {
 
-	private String nome, cpf, matricula; private Integer cod_cadastro;
+	private String nome, cpf, matricula, email, telefone, endereco;
 	
-	public aluno (String nome, String cpf, String matricula, Integer cod_cadastro){
+	public aluno (String nome, String cpf, String matricula, String email, String telefone, String endereco){
 	
 		this.nome = nome;
 		this.cpf = cpf;
 		this.matricula = matricula;
-		this.cod_cadastro = cod_cadastro;
+		this.email = email;
+		this.telefone = telefone;
+		this.endereco = endereco;
 	}
 	
 	public void setNome(String nome){
@@ -31,17 +33,34 @@ public class aluno {
 		return this.matricula; 
 	}
 	
-	public void setCod_cadastro(Integer cod){
-		this.cod_cadastro = cod;
+	public void setEmail(String email){
+		this.email= email;
 	}
-	public Integer getCod_cadastro(){
-		return this.cod_cadastro;
+	public String getEmail(){
+		return this.email; 
+	}
+
+	public void setTelefone(String telefone){
+		this.telefone= telefone;
+	}
+	public String getTelefone(){
+		return this.telefone; 
+	}
+
+	public void setEndereco(String endereco){
+		this.endereco= endereco;
+	}
+	public String getendereco(){
+		return this.endereco; 
 	}
 
 	public void DadosAluno(){
-		System.out.println("\nNome: "+this.nome);
+
+		System.out.println("\n\nNome: "+this.nome);
 		System.out.println("CPF: "+this.cpf);
 		System.out.println("Matricula: "+this.matricula);
-		System.out.println("Codigo de cadastro: "+this.cod_cadastro);
+		System.out.println("E-mail: "+this.email);
+		System.out.println("Telefone: "+this.telefone);
+		System.out.println("Endereco: "+this.endereco+"\n\n");
 	}
 }
--- cad_aluno.java
+++ cad_aluno.java
@@ -3,14 +3,11 @@ import java.util.Map;
 
 public class cad_aluno{
 
-	private static int codigo_aluno = 1;
-
 	Map<String, aluno> alu = new HashMap<String, aluno>();
 	
-	public void cadastrar(String nome, String cpf, String matricula){
-		aluno a1 = new aluno (nome, cpf, matricula, codigo_aluno);		
+	public void cadastrar(String nome, String cpf, String matricula, String email, String telefone, String endereco){
+		
+		aluno a1 = new aluno (nome, cpf, matricula, email, telefone, endereco);		
 		alu.put(cpf, a1);
-		codigo_aluno++;
 	}
-	
 }
