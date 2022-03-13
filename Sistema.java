04c2a34a3eb778f5a0aa8ea0992908bb957ca0aa
S4konji <joedsonsouza.ifba.br@gmail.com>
2022-03-13 17:38:34 -0300

    Reorganização de parte do código

--- Sistema.java
+++ Sistema.java
@@ -8,7 +8,30 @@ public class Sistema{
 
         do{
             menu.menuOpcoes();
-            menu.respostaMenu();
+
+            switch (menu.getMenu()) {
+                case 1:
+                    menu.novoAluno();
+                    break;
+                case 2:
+                    menu.consultarAluno();
+                    break;
+                case 3:
+                    menu.novoLivro();
+                    break;
+                case 4:
+                    menu.consultarLivro();
+                    break;
+                case 5:
+                    menu.livrosDisponiveis();
+                    break;
+                case 6:
+                    menu.livrosIndisponiveis();
+                    break;
+            
+                default:
+                    break;
+            }
         }while (menu.getMenu() != -1);
 
         System.out.println("\n\nOBRIGADO POR UTILIZAR NOSSO SISTEMA!\n\n");
